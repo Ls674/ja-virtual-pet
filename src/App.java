14188d10afbf4f7072d26e6ae275bdf5fa019fb4
Ls674 <lx674superz@gmail.com>
2023-05-02 21:52:20 +0000

    May 2, 2023, 5:52 PM

--- src/App.java
+++ src/App.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args, String S) throws Exception {
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print(S);
-            String name = input.nextLine();
-            System.out.println("\n"+name);
-        }
+    
+    public static void main(String[] args) {
+        Scanner myPet = new Scanner(System.in);
+        System.out.println("Enter the pet's name: ");
+        String name = myPet.nextLine();
+        System.out.println("Name: " + name);
+
+        System.out.println("What is your pets job: ");
+        String job = myPet.nextLine();
+        System.out.println("Job: " + job);
+
+        System.out.println("What is your pets age");
+        int age = myPet.nextInt();
+        myPet.nextLine();
+        System.out.println("Age: " + age);
+
+
+    }
+       
     }
 
 
-}
+
 //Comment
 
