939f93863a7ae33e84ecc34a50d13b523f84680c
Ls674 <lx674superz@gmail.com>
2023-05-02 22:24:27 +0000

    May 2, 2023, 6:24 PM

--- src/App.java
+++ src/App.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class App {
     
     public static void main(String[] args) {
-        Scanner myPet = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the characters name: ");
-        String name = myPet.nextLine();
+        String name = input.nextLine();
         System.out.println("Name: " + name);
 
         System.out.println("What is your characters job: ");
-        String job = myPet.nextLine();
+        String job = input.nextLine();
         System.out.println("Job: " + job);
 
         System.out.println("What is your characters age");
-        int age = myPet.nextInt();
-        myPet.nextLine();
+        int age = input.nextInt();
+        input.nextLine();
         System.out.println("Age: " + age);
 
+        Character myCharacter = new Character(name, job, age); 
+        System.out.println(myCharacter);
 
     }
        
