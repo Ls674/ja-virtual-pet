2fa82a27de9612c55056c9c818114260f479d9f0
Ls674 <lx674superz@gmail.com>
2023-06-04 01:18:08 +0000

    Jun 3, 2023, 9:18 PM

---
+++ bin/import discord #MTExNDcyMjM3MDY2NjYyNzEz.py
@@ -0,0 +1,10 @@
+import discord #MTExNDcyMjM3MDY2NjYyNzEzMg.GlYMZ1.QeWvurQtAwOfZbwg1qJgUR-tplrET8VKc4CbyU
+from discord.ext import commands
+intents = discord.Intents().all()
+Gilbert27 = commands.Bot(command_prefix=".",intents=intents)
+
+@Gilbert27.command()
+async def say(ctx,**message):
+    ctx.reply(message)
+
+Gilbert27.run("MTExNDcyMjM3MDY2NjYyNzEzMg.GlYMZ1.QeWvurQtAwOfZbwg1qJgUR-tplrET8VKc4CbyU")
